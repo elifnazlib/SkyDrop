b371a428ea9fff0519dafc80b18ef343aee98aed
Elif Nazlı Böke <111501335+elifnazlib@users.noreply.github.com>
2025-07-10 23:26:48 +0300

    Player Animation Added
    I made the sprite sheet for the running man with aseprite.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,49 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 10f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer playerSpriteRenderer;
+    private string lookingAt = "right";
+
+    void Start()
+    {
+        animator.speed = 2f;
+    }
+
     void Update()
     {
         Vector3 move = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            if (string.Equals(lookingAt, "right")) ;
+            else if (string.Equals(lookingAt, "left"))
+            {
+                playerSpriteRenderer.flipX = false;
+                lookingAt = "right";
+            }
+
+            animator.SetBool("isMoving", true);
             move += transform.right;
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            if (string.Equals(lookingAt, "left")) ;
+            else if (string.Equals(lookingAt, "right"))
+            {
+                playerSpriteRenderer.flipX = true;
+                lookingAt = "left";
+            }
+
+            animator.SetBool("isMoving", true);
             move -= transform.right;
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
 
         move = transform.position + playerSpeed * Time.deltaTime * move;
